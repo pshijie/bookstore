0dcc14b95ab8ffde6b4117cad37efdc21aa77444
pshijie <729589165@qq.com>
2018-11-28 00:14:27 +0800

    添加了管理员的退出与检测管理员的登录

--- src/servlet/AdminServlet.java
+++ src/servlet/AdminServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
 
@@ -29,6 +30,42 @@ public class AdminServlet extends HttpServlet {
         if("show".equals(oper)){
             //显示所有用户
             userShow(req,resp);
+            //管理员的登录
+        }else if("admlog".equals(oper)){
+            checkAdminLogin(req,resp);
+        }else if("out".equals(oper)){
+            //退出
+            adminOut(req,resp);
+        }
+    }
+
+    //管理员退出
+    private void adminOut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        HttpSession hs=req.getSession();
+        //销毁session
+        hs.invalidate();
+        //重定向到登录界面
+        resp.sendRedirect("homepage/home.html");
+    }
+
+    //检测管理员登录
+    private void checkAdminLogin(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        String admid = req.getParameter("admid");
+        String admpwd = req.getParameter("admpwd");
+        boolean is_a = as.adminCheckService(admid,admpwd);
+        if(is_a==true){
+            //获取session对象
+            HttpSession hs=req.getSession();
+            //将用户数据存储到session中
+            hs.setAttribute("admid", admid);
+            //重定向,有管理员就登录到管理员主页面
+            resp.sendRedirect("admin_main/main.jsp");
+            return;
+        }else {
+            //标识符在应用在login.jsp
+            req.setAttribute("flag",0);
+            req.getRequestDispatcher("admlogin.jsp").forward(req,resp);
+            return;
         }
     }
 
